9a0ea69767efbbfacb967f08bf6c67edc18cf3a4
abderrahmen hanafi <19hanafi93@gmail.com>
2020-05-03 21:06:20 +0400

    init commit

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 120,
+  arrowParens: 'always',
+  tabWidth: 2,
+};
---
+++ @Types/Global.d.ts
@@ -0,0 +1,6 @@
+// declare namespace NodeJs {
+//   interface Global {
+//     HermesInternal: any;
+//   }
+//   var global: Global;
+// }
